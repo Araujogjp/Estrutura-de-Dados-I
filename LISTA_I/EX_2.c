776cb49ae4544bcfaa72b463eecf6a0bcbc6000b
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2026-02-24 08:00:39 -0300

    Create EX_2.c

---
+++ LISTA_I/EX_2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    /* Variáveis */ 
+    float nota1;
+    float nota2;
+    float nota3;
+    float nota4;
+    
+    
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+
+    printf("Digite a quarta nota: ");
+    scanf("%f", &nota4);
+
+    float media = (nota1 + nota2 + nota3 + nota4)/4;
+    
+    printf("A média do aluno foi: %.2f\n", media);
+    return 0;
+}
