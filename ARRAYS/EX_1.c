c97df176f031c3d63640d0895f91a91ca8abbe24
João Pedro Araújo Gonçalves <araujoaopedro18@hotmail.com>
2026-02-24 07:49:36 -0300

    Create EX_1.c

---
+++ ARRAYS/EX_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int lista[5] = {3, 51, 18, 2, 45};
+    int i, soma = 0;
+    for(i = 0; i < 5; i++)
+        soma = soma + lista[i];
+    printf("Soma = %d\n", soma);
+    return 0;
+}
